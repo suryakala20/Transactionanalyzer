73768bfe58315afc61a8cf4917123f18e7ac216a
suryakala20 <suryakala257r@gmail.com>
2025-12-05 19:53:38 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,22 @@
-﻿using System;
-class Program
+﻿
+using System;
+namespace DeviceMonitor
 {
-    static void Main()
+    class TemperatureModule
     {
-        string transactionID = "b764pkabc23x";
-        Console.WriteLine("Transaction ID: " + transactionID);
-        decimal amount = 1000000.75m;
-        Console.WriteLine("Amount: " + amount);
-        bool isInternational = true;
-        Console.WriteLine("International: " + isInternational);
-        decimal customerRating = 2.67m;
-        Console.WriteLine("Customer Rating: " + customerRating);
-        DateTime transactionTimestamp = DateTime.Now;
-        Console.WriteLine("Timestamp: " + transactionTimestamp);
-        int rewardPoints = 200000;
-        Console.WriteLine("Reward Points: " + rewardPoints);
-
+        public static void Main(string[] args)
+        {
+            //Your Code here
+            Console.WriteLine("runnning temperaturemodule");
+            VibrationModule.Vibration();
+        }
+    }
+    class VibrationModule
+    {
+        public static void Vibration()
+        {
+            //Your code here
+            Console.WriteLine("running vibrationmodule");
+        }
     }
 }
