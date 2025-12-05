6003f57a5fed1ea0101875d7438a4a72dc61d193
suryakala20 <suryakala257r@gmail.com>
2025-12-05 19:41:27 +0530

    Add data types for transaction details
    Define data types for transaction details including ID, amount, international status, customer rating, timestamp, and reward points.

---
+++ datatypes.cs
@@ -0,0 +1,7 @@
+
+•	Transaction ID -> string
+•	Amount -> decimal // y decimal not double.double store internally 16 digits but decimal print as we do manually because we need amount without small errors
+•	IsInternational-> bool
+•	CustomerRating-> decimal
+•	Transaction Timestamp-> DateTime
+•	RewardPoints-> int
